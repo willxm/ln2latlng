899bd36b8d4b8599f85c1afea0ed9305800b054d
willxm <chenweimin74@gmail.com>
2018-08-21 11:11:32 +0800

    add area

--- localtion.go
+++ localtion.go
@@ -1,9 +1,14 @@
 package ln2latlng
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
+	"io"
+	"log"
+	"strings"
 
+	"fmt"
 	"os"
 
 	"github.com/willxm/ln2latlng/utils"
@@ -17,17 +22,43 @@ type LocationPoint struct {
 }
 
 var LocationPointData []byte
+var AreaData map[string]string
 
 func init() {
+	log.Println("init data...")
 	var err error
+	gopath := os.Getenv("GOPATH")
+	dataPath := gopath + "/src/github.com/willxm/ln2latlng/data/"
 	if LocationPointData == nil {
-		gopath := os.Getenv("GOPATH")
-		LocationPointData, err = utils.ReadFile(gopath + "/src/github.com/willxm/ln2latlng/data/location.json")
+		LocationPointData, err = utils.ReadFile(dataPath + "location.json")
 		if err != nil {
 			panic(err)
 		}
 	}
 
+	if AreaData == nil {
+		AreaData = make(map[string]string)
+
+		f, err := os.Open(dataPath + "area.txt")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		rd := bufio.NewReader(f)
+		for {
+			line, err := rd.ReadString('\n')
+
+			if err != nil || io.EOF == err {
+				break
+			}
+			line = strings.Trim(line, "\n")
+			res := strings.Split(line, "\t")
+			if len(res) > 1 {
+				AreaData[res[0]] = res[1]
+			}
+		}
+	}
 }
 
 func IdCrad2LatLng(idCard string) (string, float64, float64, error) {
@@ -39,4 +70,32 @@ func IdCrad2LatLng(idCard string) (string, float64, float64, error) {
 		}
 	}
 	return "", 0.0, 0.0, errors.New("No found location")
+}
+
+func IdCard2Area(idCard string) (string, string, string, error) {
+	areaCode2 := idCard[:2]
+	areaCode4 := idCard[:4]
+	areaCode6 := idCard[:6]
+
+	var area2, area4, area6 string
+	var err error
+
+	if v, ok := AreaData[areaCode6]; ok == true {
+		area6 = v
+	} else {
+		err = fmt.Errorf("area6 not found")
+	}
+
+	if v, ok := AreaData[areaCode4]; ok == true {
+		area4 = v
+	} else {
+		err = fmt.Errorf("area4 not found")
+	}
+
+	if v, ok := AreaData[areaCode2]; ok == true {
+		area2 = v
+	} else {
+		err = fmt.Errorf("area2 not found")
+	}
+	return area2, area4, area6, err
 }
--- localtion_test.go
+++ localtion_test.go
@@ -1,7 +1,9 @@
 package ln2latlng
 
-import "testing"
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIdCrad2LatLng(t *testing.T) {
 	type args struct {
@@ -17,9 +19,9 @@ func TestIdCrad2LatLng(t *testing.T) {
 	}{
 		// TODO: Add test cases.
 		{
-			"location",
+			"case1",
 			args{"33032619950707471X"},
-			"浙江",
+			"??",
 			120.15,
 			30.28,
 			false,
@@ -41,6 +43,50 @@ func TestIdCrad2LatLng(t *testing.T) {
 			}
 			if got2 != tt.want2 {
 				t.Errorf("IdCrad2LngLat() got2 = %v, want %v", got2, tt.want2)
+			}
+		})
+	}
+}
+
+func TestIdCard2Area(t *testing.T) {
+	type args struct {
+		idCard string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		want1   string
+		want2   string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				idCard: "33032619xxxx",
+			},
+			want:    "",
+			want1:   "",
+			want2:   "",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, got2, err := IdCard2Area(tt.args.idCard)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("IdCard2Area() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("IdCard2Area() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("IdCard2Area() got1 = %v, want %v", got1, tt.want1)
+			}
+			if got2 != tt.want2 {
+				t.Errorf("IdCard2Area() got2 = %v, want %v", got2, tt.want2)
 			}
 		})
 	}
